886c861f4695db44fbd43f06ab0a1ff5c8141d4d
Ryan Morris <MrMorris@Ryans-MacBook-Air.local>
2014-01-14 00:57:23 +0000

    add labexercise1 folder

---
+++ labexercise1/BasicJavaPrograms/src/GradeMark.java
@@ -0,0 +1,23 @@
+
+public class GradeMark {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mark;
+		
+		if (mark<40) {
+			System.out.println("Fail");
+			if (mark<59) {
+				System.out.println("Merit");
+				else
+					System.out.println("Distinction");
+			}
+		
+		}
+	}
+
+}
